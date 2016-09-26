671b55716bc55a49f122c7f8c02d0239e5192308
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-26 14:55:47 -0500

    http calls successful

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,33 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('heroController', ['$scope', '$http', function($scope, $http){
   console.log('controller');
+
+  $scope.heroes = [];
+
+  $scope.showHeroes = function(){
+    $http({
+      method:'GET',
+      url:'/heroes/hero'
+    }).then(function(results){
+      console.log(results.data);
+      $scope.heroes = results.data;
+    });
+  };
+
+  $scope.addHero = function(){
+    var objectToSend = {
+      alias: $scope.heroAlias,
+      first_name: $scope.heroFirstName,
+      last_name: $scope.heroLastName,
+      city: $scope.city,
+      power_name: $scope.power
+    };
+    console.log(objectToSend);
+    $http({
+      method:'POST',
+      url:'/heroes/hero',
+      data: objectToSend
+    }).then($scope.showHeroes);
+  };
+
 }]);
