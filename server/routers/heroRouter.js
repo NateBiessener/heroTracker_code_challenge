693cb0773e52c7c79ce8dadb31dc6526c3e511ff
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-26 14:34:00 -0500

    routes functioning

--- server/routers/heroRouter.js
+++ server/routers/heroRouter.js
@@ -7,12 +7,38 @@ var Hero = require('../models/hero');
 
 router.get('/hero', function(req, res){
   console.log('in hero get');
-  res.sendStatus(200);
+  Hero.find({}, function(err, results){
+    if (err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else {
+      res.send(results);
+    }
+  });
 });
 
 router.post('/hero', function(req, res){
   console.log('in hero post');
-  res.sendStatus(200);
+  var newHero = new Hero ({
+    alias: req.body.alias,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    city: req.body.city,
+    power_name: req.body.power_name
+  });
+
+  newHero.save(function(err){
+    console.log('saving new hero:', newHero);
+    if (err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else {
+      console.log('hero saved');
+      res.sendStatus(200);
+    }
+  });
 });
 
 module.exports = router;
